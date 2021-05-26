3e1b6453961fefe6d3d346ed96dff9b99f6a388f
Warun Sharma <warunsharma.95@gmail.com>
2021-05-26 18:10:11 +0530

    Added Question links and Description as comment

--- Problem10/first&lastOccurrenceOfAnElement.js
+++ Problem10/first&lastOccurrenceOfAnElement.js
@@ -1,3 +1,14 @@
+/*
+
+https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+*/
+
 /**
  * @param {number[]} nums
  * @param {number} target
