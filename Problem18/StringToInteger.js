2ab59b89c4812f9abd7c8c336ad634ba57b93185
Warun Sharma <warunsharma.95@gmail.com>
2021-05-31 19:14:13 +0530

    Added Problem18

---
+++ Problem18/StringToInteger.js
@@ -0,0 +1,16 @@
+
+var myAtoi = function(s) {
+    s = s.trim();
+  const max = 2 ** 31 - 1;
+  const min = -(2 ** 31);
+
+  const isNeg = s[0] === '-';
+  let res = 0;
+  for (let i = isNeg || s[0] === '+' ? 1 : 0; i < s.length; i++) {
+    if (s[i] === ' ' || Number.isNaN(s[i] - '0')) break;
+    res = res * 10 + (s[i] - '0');
+  }
+  if (isNeg) return -res < min ? min : -res;
+  return res > max ? max : res;
+};
+console.log(myAtoi("-+12"));
