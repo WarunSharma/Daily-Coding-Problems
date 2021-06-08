764cff004247041a182625ff3c51eed875c09ce3
Warun Sharma <warunsharma.95@gmail.com>
2021-06-08 06:07:56 +0530

    Added Problem29 and Problem30

---
+++ Problem30/PassByValue.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void callByValue(int x,int y){
+    int temp;
+    temp=x;
+    x=y;
+    y=temp;
+    cout<<"X:"<<x<<","<<"Y:"<<y<<"\n";
+}
+
+int main(){
+    int x,y;
+    cin>>x>>y;
+    callByValue(x,y);
+    cout<<"X:"<<x<<","<<"Y:"<<y;
+    return 0;
+}
+
