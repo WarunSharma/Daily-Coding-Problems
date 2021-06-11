9058af0aae5ce60af7751de99fbf1bdeedaa5f65
Warun Sharma <warunsharma.95@gmail.com>
2021-06-11 22:54:44 +0530

    Added optimal solution for Array Rotation

---
+++ Problem34/ArrayRightRotation_2.js
@@ -0,0 +1,29 @@
+function rightRotation(arr,places){
+    places%=arr.length;
+    if(places==0){
+        return arr;
+    }
+
+    for(let i=0,j=arr.length-1;i<j;++i,--j){
+        let temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+
+    for(let i=0,j=places-1;i<j;++i,--j){
+        let temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    for(let i=places,j=arr.length-1;i<j;++i,--j){
+        let temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    return arr;
+}
+
+console.log(rightRotation([1,2,3,4,5],2));
+console.log(rightRotation([1,2,3,4,5],0));
+console.log(rightRotation([1,2,3,4,5],5));
+console.log(rightRotation([1,2,3,4,5],4));
