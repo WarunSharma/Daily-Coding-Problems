8e21102b8d7f15ef013b3785deb5dcf195cc9ebf
Warun Sharma <warunsharma.95@gmail.com>
2021-06-12 22:57:23 +0530

    Added problem statement in Problem35

--- Problem35/MajorityElement_2.js
+++ Problem35/MajorityElement_2.js
@@ -1,3 +1,12 @@
+/*
+
+https://leetcode.com/problems/majority-element/
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+*/
+
 function getMajorityElement(arr){
     if(arr.length==1){
         return arr[0];
