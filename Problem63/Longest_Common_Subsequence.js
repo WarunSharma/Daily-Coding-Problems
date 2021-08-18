2514926f285907aa4aab27aef15adbe0d401dc54
Warun Sharma <warunsharma.95@gmail.com>
2021-08-18 18:01:48 +0530

    Modified function name

--- Problem63/Longest_Common_Subsequence.js
+++ Problem63/Longest_Common_Subsequence.js
@@ -1,15 +1,15 @@
 
-function lowestCommonSubsequence(str1,str2,m,n){
+function longestCommonSubsequence(str1,str2,m,n){
     if(m==0 || n==0){
         return 0;
     }
 
     if(str1[m-1]===str2[n-1]){
-        return 1 + lowestCommonSubsequence(str1,str2,m-1,n-1);
+        return 1 + longestCommonSubsequence(str1,str2,m-1,n-1);
     }
     else{
-        return Math.max(lowestCommonSubsequence(str1,str2,m-1,n),lowestCommonSubsequence(str1,str2,m,n-1));
+        return Math.max(longestCommonSubsequence(str1,str2,m-1,n),longestCommonSubsequence(str1,str2,m,n-1));
     }
 }
 
-console.log(lowestCommonSubsequence('AXYZ','BAZ',4,3));
+console.log(longestCommonSubsequence('AXYZ','BAZ',4,3));
