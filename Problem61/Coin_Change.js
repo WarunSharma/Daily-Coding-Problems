38d2c2f18f5c04e41472ba57c3061ed5b527e314
Warun Sharma <warunsharma.95@gmail.com>
2021-08-20 22:00:44 +0530

    Added comments in recursive solution of Coin Change problem

--- Problem61/Coin_Change.js
+++ Problem61/Coin_Change.js
@@ -6,16 +6,20 @@ https://www.geeksforgeeks.org/coin-change-dp-7/
 let min=Number.MAX_SAFE_INTEGER;
 function coinChange(N,coins,coinSum,coinStore=[],coinCount=0){
 
+    //Return if coin sum is greater than N
     if(coinSum>N){
         return;
     }
 
+    //Coin Sum is equal to N
     if(coinSum==N){
         console.log(coinStore);
+        //Calculate minimum coin count
         min=Math.min(min,coinCount);
         return;
     }
 
+    //Loop through the coins
     for(let coin of coins){
         coinStore.push(coin);
         coinChange(N,coins,coinSum+coin,coinStore,coinCount+1);
